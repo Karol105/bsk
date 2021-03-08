6102f62a5eb8691e4e6c97a48d1c12b804728077
Karol <karolnever@wp.pl>
2021-03-08 17:05:25 +0100

    Testy jednostkowe Rail fence

---
+++ src/com/example/bsk/RailFenceTest.java
@@ -0,0 +1,29 @@
+package com.example.bsk;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RailFenceTest {
+    private RailFence railFence;
+
+    @Test
+    public void railtest1() {
+        railFence = new RailFence(3);
+        Assert.assertEquals("CTARPORPYYGH",
+                railFence.getEncryptedData("CRYPTOGRAPHY"));
+    }
+
+    @Test
+    public void railtest2() {
+        railFence = new RailFence(2);
+        Assert.assertEquals("KRLAO",
+                railFence.getEncryptedData("KAROL"));
+    }
+
+    @Test
+    public void railtest3() {
+        railFence = new RailFence(5);
+        Assert.assertEquals("EXERCISMISAWESOME",
+                railFence.getDecryptedData("EIEXMSMESAORIWSCE"));
+    }
+}
